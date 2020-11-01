688e6b8ae4e36a1337b660bf63b2e4f6e5d1226d
JHLim <kor.limj@gmail.com>
2020-11-01 21:45:40 +0900

    update comment

--- app/crawl.py
+++ app/crawl.py
@@ -2,37 +2,49 @@
 ## pip install pandas
 
 from Bio import Entrez
+## 전체 코드의 핵심 library, Entrez 데이터베이스를 crawl 할 수 있는 api 
+## https://biopython.org/docs/1.75/api/Bio.Entrez.html 참고 
 import pandas as pd
 from timeit import default_timer as timer
 from urllib.error import HTTPError
 import time
 
+## Gene list 불러오기 
 gene_data = pd.read_csv('static/data/gene_with_protein_product.txt', sep = '\t')
 genes = list(gene_data['symbol'])
 
 def getRelations(ids, src):
+    ## 1. id 에 해당하는 article들을 불러와서 
+    ## 2. 두개의 유전자(src와 다른 하나)가 포함된 문장들을 저장하기 위한 함수 
     res = []
     try:
         handle = Entrez.efetch(db='pubmed',
                                retmode='xml',
                               id=ids)
         results = Entrez.read(handle)
+        ## id에 해당하는 article들을 불러옴, Dictionary 형태로 저장됨 
     except:
         return res
     
     for paper in results['PubmedArticle']:
         try:
             curTexts = paper['MedlineCitation']['Article']['Abstract']['AbstractText']
+            ## Abstracttext만 긁어오기 
             PMID = str(paper['MedlineCitation']['PMID'])
+            ## PMID도 함께 저장 ( 추가됨 )
             sentences = []
             abs = ""
             for para in curTexts:
+                ## Abstract가 각 문장별로 저장되어있는경우와 아닌 경우가 있음( JAMA style, Nature style )
+                ## 문장단위로 분할하여 저장 
                 sentences.extend(para.strip(' .').split('.'))
                 abs += para.strip(' .').split('.')[0] + ". "
             for cur in sentences:
                 words = cur.strip().split(' ')
+                ## 문장을 다시 단어단위로 나누어서 
                 if src in words:
                     for tar in genes:
+                        ## 두개의 Gene이 한 문장에 들어있는 경우만 저장하기 
                         if src == tar:
                             continue
                         if tar in words:
@@ -44,11 +56,21 @@ def getRelations(ids, src):
     return res
 
 if __name__ == "__main__":
+    """
+        1. 특정 유전자로 검색되는 article의 id를 받아서 
+        2. getRelations 함수를 통해 해당 gene과 다른 gene이 동시에 등장하는 문장을 검색하고 
+        3. 이를 액셀에 저장 ( entrez api는 속도가 빠른 편이 아니기 때문에, 중간중간 다른 이름으로 저장하였음 )
+    """
     setAll = set()
+    ### crawl 시간을 줄이기 위한 방법. 이전에 탐색하지 않은 article만 분석하기 위해 set 사용 
     resSen = []
+    ### 결과 저장을 위한 변수, 진짜 결과 
     meta = []
+    ### 결과 저장을 위한 변수 222, 검색한 article 수, 유전자 수 등 meta 변수들을 저장함 
     totalQ = 0
+    ### 결과 저장을 위한 변수 333, 현재까지 검색한 article 수  
     idx = 0
+    ### 중간 저장을 위해 idx를 하나씩 더해가고, 500 단위로 중간 저장 
 
     start = timer()
 
@@ -58,7 +80,9 @@ if __name__ == "__main__":
         try:
             handle = Entrez.esearch(db='pubmed', retmode='xml', restatrt = idx, retmax = 100000, term=src)
             results = Entrez.read(handle)
+            ### src gene을 가진 article들의 id를 받아옴 
         except HTTPError:
+            ### 간혹 너무 많은 query를 날려서 entrez 서버에서 505에러를 뱉어내는 경우가 있음, 이 경우 5초 뒤에 다시 쿼리하면 됨 
             print('ha....')
             time.sleep(5)
             handle = Entrez.esearch(db='pubmed', retmode='xml', restatrt = idx, retmax = 100000, term=src)
@@ -67,10 +91,13 @@ if __name__ == "__main__":
         IdSet = set(results['IdList'])
         
         curSearch = IdSet - setAll
+        ### 기존에 쿼리하지 않은 article만 query하기 위해 차집합 사용 
         
         tmp = getRelations(curSearch, src)
+        ### getRelations 함수를 통해 결과 받아옴 
         resSen.extend(tmp)
         
+        ### 아래는 결과 저장을 위한 코드임 
         setAll.update(IdSet)
         totalQ += len(IdSet)
         end = timer()
---
+++ app/network.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+    1. Crawl 결과에서 positive 텀과 negative 텀이 포함된 문장을 다시 골라내어 이들을 network 형태로 저장 
+    2. Jacard distance를 계산하여 relation을 prediction 하고, 
+    3. 특정 threshold를 넘는 relation만 선정하여 pickle 형태로 저장 
+"""
+
+tt = pd.read_excel('static/data/Crawl.xlsx')
+
+positives = ["prolong", "regulator", "accelerate", "modulate", "enhance", "promote", "interact", "up-regulate"]
+negatives = ["down-regulate", "suppress", "prevent", "inhibit", "exploit"]
+
+posis = []
+negas = []
+
+for index, row in tt.iterrows():
+    ## positive 또는 negative term을 가지고 있는 문장일 경우 저장 
+    geneA = row[0]
+    geneB = row[1]
+    string = row[2]
+    if any(ele for ele in positives if(ele in string)):
+        re = [geneA, geneB]
+        posis.append(re)
+    if any(ele for ele in negatives if(ele in string)):
+        re = [geneA, geneB]
+        negas.append(re)
+
+G = nx.Graph()
+## networkx 객체 생성 
+
+for edge in posis:
+    ## positive edge는 weight를 1씩 더해주고 
+    if edge in G.edges():
+        G[edge[0]][edge[1]]['weight'] += 1
+    else:
+        G.add_edge(edge[0], edge[1], weight = 1)
+
+for edge in negas:
+    ## negative edge는 weight를 1씩 감소시킴 
+    if edge in G.edges():
+        G[edge[0]][edge[1]]['weight'] -= 1
+    else:
+        G.add_edge(edge[0], edge[1], weight = -1)
+
+nx.write_gpickle(G, "Whole.pickle")
+## network 저장 
+
+preds = nx.jaccard_coefficient(G) ## Jacard distance 계산 
+count = 0
+ps = []
+epred = []
+res = []
+for u, v, p in preds:
+    count += 1 
+    if count % 1000000 == 0:
+        print(count, "edges calculated")
+    if p > 0:
+        res.append([u, v, p])
+
+ress = pd.DataFrame(res, index = None)
+ress.to_csv("Jacard.csv", header = None, index = None)
+
+J = nx.Graph()
+for index, row in ress.iterrows():
+    if row[2] > 0.05:  ## Jacard distance가 0.05 이상인 경우를 prediction 
+        J.add_edge(row[0], row[1], dist = row[2])
+
+nx.write_gpickle(J, "Jacard.pickle")
