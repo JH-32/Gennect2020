4fa01f9b1abf37b87304d7d71d268a4f2057a268
JHLim <kor.limj@gmail.com>
2020-11-03 21:02:56 +0900

    add button

--- app/main.py
+++ app/main.py
@@ -29,6 +29,57 @@ def result():
     tGene = request.form['gene']
 
     G = nx.read_gpickle("static/data/Whole.pickle")
+
+    print("Set genes for visualization..")
+    target = [tGene]
+    target.extend(list(G.neighbors(target[0])))
+    H = G.subgraph(target)
+    del G 
+
+    ## positive, negative, neutral link에 대해 각각 시각화 
+    eposi = [(u, v) for (u, v, d) in H.edges(data=True) if d["weight"] > 0]
+    eposiweight = np.log([d['weight'] for (u, v, d) in H.edges(data=True) if d["weight"] > 0])
+    resTablePosi = []
+    for [u, v] in eposi:
+        try:
+            for [sen, pmid] in resData[u][v]:
+                resTablePosi.append([u, v, sen, pmid])
+        except KeyError:
+            continue
+
+    enega = [(u, v) for (u, v, d) in H.edges(data=True) if d["weight"] < 0]
+    enegaweight = np.log(np.negative([d['weight'] for (u, v, d) in H.edges(data=True) if d["weight"] < 0]))
+    resTableNega = []
+    for [u, v] in enega:
+        for [sen, pmid] in resData[u][v]:
+            resTableNega.append([u, v, sen, pmid])
+    
+    eneu = [(u, v) for (u, v, d) in H.edges(data=True) if d["weight"] == 0]
+    resTableNeu = []
+    for [u, v] in eneu:
+        for [sen, pmid] in resData[u][v]:
+            resTableNeu.append([u, v, sen, pmid])
+
+    timestr = time.strftime("%Y%m%d_%H%M%S")
+    figName = "static/fig/" + tGene + timestr + ".png"
+    plt.figure(figsize=(20, 20))
+    pos = nx.circular_layout(H)
+    nx.draw_networkx_nodes(H, pos, node_size=20)
+    nx.draw_networkx_labels(H, pos, font_size=30, font_family="sans-serif")
+    nx.draw_networkx_edges(H, pos, edgelist = eposi, width=eposiweight, edge_color="r")
+    nx.draw_networkx_edges(H, pos, edgelist = enega, width=enegaweight, edge_color="b")
+    nx.draw_networkx_edges(H, pos, edgelist = eneu, width=enegaweight, edge_color="y")
+
+    plt.savefig(figName, format="PNG", dpi=300)
+
+    return render_template('result.html', fName = figName, tposi = resTablePosi, tnega = resTableNega, tneu = resTableNeu)
+
+
+@app.route('/result_ai', methods=['POST']) # 결과 URL
+def resultai():
+    tGene = request.form['gene']
+
+    G = nx.read_gpickle("static/data/Whole.pickle")
     J = nx.read_gpickle("static/data/Jacard.pickle")
 
     print("Set genes for visualization..")
@@ -84,30 +135,10 @@ def result():
     nx.draw_networkx_edges(H, pos, edgelist = eneu, width=enegaweight, edge_color="y")
     nx.draw_networkx_edges(H, pos, edgelist = epredf, edge_color="g", style = "dashed")
 
-    ##img = BytesIO()
-    ##plt.savefig(img, format="PNG", dpi=300)
-    ##img.seek(0)
-
-    #return send_file(img, mimetype='image/png')
-
     plt.savefig(figName, format="PNG", dpi=300)
 
-    # img = BytesIO()
-    # plt.savefig(img, format="PNG", dpi=300)
-
-    ## 우선은 이미지를 gennect2020image 버킷에서 다운로드, gcloud app engine은 read only 이기 때문임 
-    # CLOUD_STORAGE_BUCKET = "gennect2020image"
-    # gcs = storage.Client()
-    # bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
-    # blob = bucket.blob(figName)
-    # blob.upload_from_string(
-    #     img.getvalue(),
-    #     content_type='image/png')
-    # img.close()
-
-    # url = blob.public_url
-
     return render_template('result.html', fName = figName, tpred = resTablePred, tposi = resTablePosi, tnega = resTableNega, tneu = resTableNeu)
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
